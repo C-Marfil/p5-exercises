7e58a149a0d749895d1928b7aaad79a8e5487ec9
Carlos Marfil <carlos.marfil.r@gmail.com>
2023-04-30 21:06:21 +0100

    played with animations

--- sketch.js
+++ sketch.js
@@ -1,12 +1,24 @@
+let t;
+let increment = 0.001; 
+let starX = 0;
+let starY = 0;
+let starSize = 6;
+let starSpeed = 0;
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
+  frameRate(24);
+  strokeWeight(2);
+  stroke(255);
+  noFill();
+  t = 0;
 }
 
 function draw() {
-  background(0);
+  background(0, 16);
   
   for (let li = 0; li < 80; li++) { 
-    stroke(li * 25);
+    stroke(li * 3);
     strokeWeight(100 - li);
     line(0 + li * 50,0 + li * 5,400 - li * 50,400 + li * 50);
   }
@@ -21,5 +33,37 @@ function draw() {
     noStroke();
     fill(255 - t, 255 - t * 3, 0);
     triangle(300 - t * 75 , 300 - t, 600 + t, 600 + t, 600, 650 + t);
+  }
+
+  makeSparkle();
+  makeCircle(4, 50, 600);
+  makeCircle(0, 200, 200);
+  makeCircle(100, 200, 1);
+  shootingStar();
+
+  function shootingStar() {
+    circle(starX, starY, starSize);
+    starX += 1;
+    starY += 1;
+  }
+
+  function makeSparkle() {
+    stroke(255);
+    noFill();
+    strokeWeight(2);
+    circle(random(width), random(height), random(200));
+
+  }
+  function makeCircle(xPos, yPos, circleSize) { 
+    stroke(255);
+    strokeWeight(22);
+    noFill();
+    let x = width * noise(t + xPos);
+    let y = height * noise(t + yPos);
+    let size = width * noise(t + circleSize);
+  
+    ellipse(x, y, size, size);
+  
+    t += increment;
   }
 }
