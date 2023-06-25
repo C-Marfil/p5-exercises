10535f430ec4e8cd24e6de24be0500b38bb7d406
Carlos Marfil <carlos.marfil.r@gmail.com>
2023-06-25 19:54:02 +0100

    added pop and push to helper

--- helper.js
+++ helper.js
@@ -1,8 +1,11 @@
 export function fields(){
     noStroke();
     fill("darkgreen");
+    push()
     ellipse(width / 8, height, 800, 300);
+    pop()
     ellipse(width / 2, height, 600, 400);
+    pop()
     ellipse(width, height, 1100, 400);
     ellipse(width / 3, height, 1100, 400);
   }
